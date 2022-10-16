59671f52ee3e13ea26fca38c16bb468046be5f55
ZUBOV ILLIA <zubovillia@gmail.com>
2022-10-16 13:58:57 +0300

    installed hook use form, small form to practice, result in console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useForm } from "react-hook-form";
 
-function App() {
+export default function App() {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset
+  } = useForm({
+    mode: "all"
+  });
+
+  const onSubmit = (data) => {
+    // alert(JSON.stringify(data));
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Hook Form</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          First Name:
+          <input
+            {...register("firstName", {
+              required: {
+                value: true,
+                message: "поле обязательно к заполнению"
+              },
+              minLength: {
+                value: 5,
+                message: "Минимум 5 символов"
+              }
+            })}
+          />
+        </label>
+        <div style={{ height: 40 }}>
+          {errors?.firstName && <p>{errors?.firstName?.message || "Error!"}</p>}
+        </div>
+
+        <label>
+          First Name:
+          <input
+            {...register("lastName", {
+              required: {
+                value: true,
+                message: "поле обязательно к заполнению"
+              },
+              minLength: {
+                value: 5,
+                message: "Минимум 5 символов"
+              }
+            })}
+          />
+        </label>
+        <div style={{ height: 40 }}>
+          {errors?.lastName && <p>{errors?.lastName?.message || "Error!"}</p>}
+        </div>
+
+        <input type="submit" disabled={!isValid} />
+      </form>
     </div>
   );
 }
-
-export default App;
